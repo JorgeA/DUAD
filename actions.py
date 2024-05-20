7e12aa7b8b9efbd6ee9352b07235c8c1bbb821a8
JorgeA <arguedas.jorge89@gmail.com>
2024-05-20 16:16:51 -0600

    Fixed the issues about score validation.

--- actions.py
+++ actions.py
@@ -1,37 +1,41 @@
 students = []
 
-
 def add_student():
     while True:
         try:
             student = {}
-            student["name"]=input("Student's full name: ")
-            student["group"]=input("Student's group number: ")
-            student["spanish_score"]=int(input("Student's spanish score: "))
-            student["spanish_score"]=validate_score(student["spanish_score"])
-            student["english_score"]=int(input("Student's english score: "))
-            student["english_score"]=validate_score(student["english_score"])
-            student["scienses_score"]=int(input("Student's sciences score: "))
-            student["scienses_score"]=validate_score(student["scienses_score"])
-            student["social_studies_score"]=int(input("Student's social studies score: "))
-            student["social_studies_score"]=validate_score(student["social_studies_score"])
-            student["score_avg"]= (int(student["spanish_score"])+int(student["english_score"])+int(student["social_studies_score"])+int(student["scienses_score"])) /4
-            print("")
-            print("Student successfully added!!")
+            student["name"] = input("Nombre completo del estudiante: ")
+            student["group"] = input("Numero de Seccion del estudiante: ")
+            student["spanish_score"] = validate_score(input("Ingrese la nota de Español: "))
+            student["english_score"] = validate_score(input("Ingrese la nota de Ingles: "))
+            student["scienses_score"] = validate_score(input("Ingrese la nota de Ciencias: "))
+            student["social_studies_score"] = validate_score(input("Ingrese la nota de Estudios Sociales: "))
+            student["score_avg"] = (
+                int(student["spanish_score"]) + 
+                int(student["english_score"]) + 
+                int(student["social_studies_score"]) + 
+                int(student["scienses_score"])
+            ) / 4
+            print("\nEstudiante agregado satisfactoriamente!!")
             students.append(student)
             break
         except ValueError as error:
             print(f'Ha ocurrido un error: {error}')
             print("Ingrese una nota en formato valido")
-            error_field = str(error).split("'")[1]
-            student[error_field] = int(input(f"Corrija el valor de {error_field} por una nota valida: "))
-            students.append(student)
             break
+
+
 def validate_score(score):
-    if 0 < score > 100:
-        print("Pro favor ingrese una notra valida entre 0 y 100")
-        score = input("Student's correct score: ")
-    return score
+    while True:
+        if not score.isdigit():
+            print("Por favor ingrese una nota valida. Sin letras")
+        else:
+            score = int(score)
+            if 0 <= score <= 100:
+                return score
+            else:
+                print("Por favor ingrese una nota valida entre 0 y 100")
+        score = input("Ingrese la nota correcta: ")
 
 
 def show_all_students():
--- testing.py
+++ testing.py
@@ -1,36 +1,5 @@
-students = []
-
-
-def add_student():
-    while True:
-        try:
-            student = {}
-            student["name"]=input("Student's full name: ")
-            student["group"]=input("Student's group number: ")
-            student["spanish_score"]=int(input("Student's spanish score: "))
-            student["spanish_score"]=validate_score(student["spanish_score"])
-            student["english_score"]=int(input("Student's english score: "))
-            student["english_score"]=validate_score(student["english_score"])
-            student["scienses_score"]=int(input("Student's sciences score: "))
-            student["scienses_score"]=validate_score(student["scienses_score"])
-            student["social_studies_score"]=int(input("Student's social studies score: "))
-            student["social_studies_score"]=validate_score(student["social_studies_score"])
-            student["score_avg"]= (int(student["spanish_score"])+int(student["english_score"])+int(student["social_studies_score"])+int(student["scienses_score"])) /4
-            print("")
-            print("Student successfully added!!")
-            students.append(student)
-            break
-        except ValueError as error:
-            print(f'Ha ocurrido un error: {error}')
-            print("Ingrese una nota en formato valido")
-            error_field = str(error).split("'")[1]
-            #student[error_field] = int(input(f"Corrija el valor de {error_field} por una nota valida: "))
-            #students.append(student)
-            #break
-def validate_score(score):
-    if 0 < score > 100 or score.isdigit() != True:
-        print("Pro favor ingrese una notra valida entre 0 y 100")
-        score = input("Student's correct score: ")
-    return score
-
-add_student()
+score = input("Student's correct score: ")
+if score.isdigit() == True:
+    print("Por favor ingrese una nota valida. Sin letras")
+    score = input("Student's correct score: ")
+    print(score) 
