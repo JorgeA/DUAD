b0f3337c395a5cdc75b19e4e600b5d37e07db429
JorgeA <arguedas.jorge89@gmail.com>
2024-07-31 10:26:48 -0600

    Added second buble sort exercise

---
+++ Week_15/Buble_sort_right_to_left.py
@@ -0,0 +1,10 @@
+list_to_order = [5, 3, 8, 6, 1, 9, 2, 7]
+
+def bubble_sort_descending(list_to_order):
+    for index_outside in range(len(list_to_order)):
+        for index_inside in range(len(list_to_order) - 1):
+            if list_to_order[index_inside] < list_to_order[index_inside + 1]:
+                list_to_order[index_inside], list_to_order[index_inside + 1] = list_to_order[index_inside + 1], list_to_order[index_inside]
+    return list_to_order
+
+print(bubble_sort_descending(list_to_order))
