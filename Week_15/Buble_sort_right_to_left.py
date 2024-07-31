6dd321e96356e0f665a21484e5f246214de5b95c
JorgeA <arguedas.jorge89@gmail.com>
2024-07-31 13:24:14 -0600

    Adde new exercises week 15

--- Week_15/Buble_sort_right_to_left.py
+++ Week_15/Buble_sort_right_to_left.py
@@ -1,10 +1,10 @@
-list_to_order = [5, 3, 8, 6, 1, 9, 2, 7]
+list_to_order = [5, 3, 8, 6, 1, 9, 2, 7] # O(1)
 
 def bubble_sort_descending(list_to_order):
-    for index_outside in range(len(list_to_order)):
-        for index_inside in range(len(list_to_order) - 1):
-            if list_to_order[index_inside] < list_to_order[index_inside + 1]:
-                list_to_order[index_inside], list_to_order[index_inside + 1] = list_to_order[index_inside + 1], list_to_order[index_inside]
-    return list_to_order
+    for index_outside in range(len(list_to_order)):# O(n)
+        for index_inside in range(len(list_to_order) - 1):# O(log n)
+            if list_to_order[index_inside] < list_to_order[index_inside + 1]: # O(1)
+                list_to_order[index_inside], list_to_order[index_inside + 1] = list_to_order[index_inside + 1], list_to_order[index_inside] # O(1)
+    return list_to_order # O(1)
 
-print(bubble_sort_descending(list_to_order))
+print(bubble_sort_descending(list_to_order)) # O(1)
